f4802b5fe7d78afbea3773d0f12947a33b342596
lidiagaldino <lidiagabrielly14@gmail.com>
2024-09-27 16:42:57 -0300

    feat: add post controller

---
+++ src/main/java/br/com/lidiagaldino/feed/controller/PostController.java
@@ -0,0 +1,49 @@
+package br.com.lidiagaldino.feed.controller;
+
+import br.com.lidiagaldino.feed.application.dto.CreatePostDto;
+import br.com.lidiagaldino.feed.application.usecases.CreatePostUsecase;
+import br.com.lidiagaldino.feed.application.usecases.FindAllPostsUsecase;
+import br.com.lidiagaldino.feed.application.usecases.FindPostByAuthorUsecase;
+import br.com.lidiagaldino.feed.domain.entities.Post;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("posts")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class PostController {
+
+    @Inject
+    private final CreatePostUsecase createPostUsecase;
+    @Inject
+    private final FindAllPostsUsecase findAllPostsUsecase;
+    @Inject
+    private final FindPostByAuthorUsecase findPostByAuthorUsecase;
+
+    public PostController(CreatePostUsecase createPostUsecase, FindAllPostsUsecase findAllPostsUsecase, FindPostByAuthorUsecase findPostByAuthorUsecase) {
+        this.createPostUsecase = createPostUsecase;
+        this.findAllPostsUsecase = findAllPostsUsecase;
+        this.findPostByAuthorUsecase = findPostByAuthorUsecase;
+    }
+
+
+    @GET
+    public Multi<Post> index() {
+        return findAllPostsUsecase.execute();
+    }
+
+    @GET
+    @Path("author/{name}")
+    public Multi<Post> findByAuthor(@PathParam("name") String name) {
+        return findPostByAuthorUsecase.execute(name);
+    }
+
+    @POST
+    public Uni<Post> store(CreatePostDto post) {
+        return createPostUsecase.execute(post);
+    }
+
+}
